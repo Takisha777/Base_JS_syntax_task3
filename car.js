321c484a99136875c994d3327665845c51bc931a
Takisha777 <alexey.kovtun.od@gmail.com>
2024-05-16 02:20:53 +0200

    Base_JS_syntax_task3

---
+++ car.js
@@ -0,0 +1,20 @@
+const carName = 'Nissan';
+let maxCarSpeed = 165,
+    carOwner;
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = 200;
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = 300;
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = 220;
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+carOwner = 'Max';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'cat';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Peter';
+console.log('У машины изменился владелец', carOwner);
